5133d26040224489a8243e600eeef5ec82ab61b4
Robby Evans <ruttoevans001@gmail.com>
2024-09-13 09:33:17 +0300

    minor fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@components": path.resolve(__dirname, "src/components/"),
-      "@styles": path.resolve(__dirname, "src/components/"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@styles": path.resolve(__dirname, "src/styles"),
     },
   },
   server: {
